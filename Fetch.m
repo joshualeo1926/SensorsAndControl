9769aeef5aef4e3bc0ea5a4e5a7b4ae7180569d9
Jonathan <wildejono@gmail.com>
2020-09-30 15:34:27 +1000

    Added laser data ROS connectivity within FETCH Added get_laser_data within class FETCH

--- Fetch.m
+++ Fetch.m
@@ -7,11 +7,16 @@ classdef Fetch < handle
         function self = Fetch(self)
             rosshutdown;
             rosinit;
-            self.img_sub = rossubscriber('/head_camera/rgb/image_raw', 'sensor_msgs/Image');           
+            self.img_sub = rossubscriber('/head_camera/rgb/image_raw', 'sensor_msgs/Image');
+            self.laser_sub = rossubscriber('/base_scan_raw', 'sensor_msgs/LaserScan');
         end
         
         function rgb_data = get_rgb_image(self)
             rgb_data = receive(self.img_sub);
+        end
+        
+        function laser_data = get_laser_data(self)
+            laser_data = receive(self.laser_sub);
         end
     end
 end
